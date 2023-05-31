f13a3407e11d0c14cc023f43dcdbe3261e579793
Luís Gustavo <gustavovacodar@gmail.com>
2023-05-31 11:25:28 -0300

    adjustment in project structure

---
+++ resources/views/notepad/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.index')
+
+@section('title', 'My Notes')
+
+@section('content')
+    <ul class="list-group">
+        <li class="list-group-item disabled">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Morbi leo risus</li>
+        <li class="list-group-item">Porta ac consectetur ac</li>
+        <li class="list-group-item">Vestibulum at eros</li> 
+    </ul>
+@endsection
